604e9043aa8f0ce77d2673272433ca4af3c6e5e1
Dániel Korompai <korompaidani@gmail.com>
2023-03-04 18:28:03 +0100

    Update timeline command and its handler is added

---
+++ StarWars.DataTank.Application/Features/Timelines/Commands/UpdateTimeline/UpdateTimelineCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using StarWars.DataTank.Application.Contracts.Persistence;
+using StarWars.DataTank.Domain.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StarWars.DataTank.Application.Features.Timelines.Commands.UpdateTimeline
+{
+    public class UpdateTimelineCommandHandler : IRequestHandler<UpdateTimelineCommand>
+    {
+        private readonly IAsyncRepository<Timeline> _timelineRepository;
+
+        public UpdateTimelineCommandHandler(IAsyncRepository<Timeline> timelineRepository, IMapper mapper)
+        {
+            _timelineRepository = timelineRepository;
+        }
+
+        async public Task<Unit> Handle(UpdateTimelineCommand request, CancellationToken cancellationToken)
+        {
+            var timeline = await _timelineRepository.GetByIdAsync(request.TimelineId);
+
+            if (timeline == null)
+            {
+                //TODO: Throw an error if not found
+            }
+
+            await _timelineRepository.UpdateAsync(timeline);
+
+            return Unit.Value;
+        }
+    }
+}
