ee7f1712e44d8ce78dfbe55542e080ea9201fb6c
Dániel Korompai <korompaidani@gmail.com>
2023-03-04 09:46:06 +0100

    Baseic contracts are added to Application core

---
+++ StarWars.DataTank.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StarWars.DataTank.Application.Contracts.Persistence
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> AddAsync(T model);
+        Task UpdateAsync(T model);
+        Task DeleteAsync(T model);
+    }
+}
---
+++ StarWars.DataTank.Application/Contracts/Persistence/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using StarWars.DataTank.Domain.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StarWars.DataTank.Application.Contracts.Persistence
+{
+    public interface ICategoryRepository : IAsyncRepository<Category>
+    {
+        Task<List<Category>> GetCategoriesWithTimeLines();
+    }
+}
