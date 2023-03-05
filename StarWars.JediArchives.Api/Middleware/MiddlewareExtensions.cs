27a57f05d2296e3e917eabc4bd871d09c2edf396
Dániel Korompai <korompaidani@gmail.com>
2023-03-05 13:24:14 +0100

    Exception handler middleware is introduced to pipeline

---
+++ StarWars.JediArchives.Api/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace StarWars.JediArchives.Api.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionHandlerMiddleware>();
+        }
+    }
+}
