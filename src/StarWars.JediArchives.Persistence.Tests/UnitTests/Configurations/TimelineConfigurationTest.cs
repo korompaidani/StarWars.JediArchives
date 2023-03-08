43cbb3bc3f58aaeb44a397997cf6361ef73774d1
Dániel Korompai <korompaidani@gmail.com>
2023-03-08 20:02:45 +0100

    Moq added to testprojects

--- src/StarWars.JediArchives.Persistence.Tests/UnitTests/Configurations/TimelineConfigurationTest.cs
+++ src/StarWars.JediArchives.Persistence.Tests/UnitTests/Configurations/TimelineConfigurationTest.cs
@@ -3,9 +3,23 @@ namespace StarWars.JediArchives.Persistence.Tests.UnitTests.Configurations
     [TestClass]
     public class TimelineConfigurationTest
     {
-        [TestMethod]
-        public void TestConfigure()
+        private Mock<IInfrastructure<IConventionEntityTypeBuilder>> _entityTypeConfigurationMock;
+
+        [TestInitialize]
+        public void TestInitialize()
         {
+            _entityTypeConfigurationMock = new Mock<IInfrastructure<IConventionEntityTypeBuilder>>();
+        }
+
+        [TestMethod]
+        public void Build_EntityTypeConfiguration_ShouldBeResultsIsRequiredSet()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.Fail("NOT TESTED YET!");
         }
     }
 }
--- src/StarWars.JediArchives.Persistence.Tests/Usings.cs
+++ src/StarWars.JediArchives.Persistence.Tests/Usings.cs
@@ -1,2 +1,4 @@
+global using Microsoft.EntityFrameworkCore.Infrastructure;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using StarWars.JediArchives.Persistence.Configurations;
+global using Moq;
